7845fe2e59ac1202b80b292bb7bcc78a0f6c6639
Carlos González <cgonzalezsan@serikat.es>
2017-01-12 12:46:18 +0100

    Actualizar los sql de pie y cabecera con los últimos cambios para hacerlos compatible con el generador 0.6.0 y 0.6.1 y así poder usarlos par MI Se va a escribir siempre el "set @rc = 0" y el ";THROW" y a mano habrá que quitarlos de donde no hagan falta:  -dims y fact --> quitar el set @rc = 0  -callers --> quitar el ;throw  -resto de casos --> quitar ambos

--- cambios_para_ssis_solo_cabecera.sql
+++ cambios_para_ssis_solo_cabecera.sql
@@ -2,29 +2,32 @@ AS
 BEGIN
 
 --Inicio cabecera--
+
 	SET NOCOUNT ON
 
 	BEGIN TRY
 
-		DECLARE	@bd					varchar(50)	= 'dbn1_stg_dhyf',
-				@esquema			varchar(50)	= 'dbo',
-				@objeto				varchar(200)= 'spxxx',
-				@fecha_inicio		datetime	= GETDATE(),
-				@num_registros		int			= NULL,
-				@id					int			= NULL,
-				@rc					int,
-				@count_all			int,
-				@count_ins			int,
-				@idx_reclim			int
+		--declaración de variables
+		DECLARE	@bd varchar(50) = 'dbn1_stg_dhyf',
+				@esquema varchar(50) = 'dbo',
+				@objeto varchar(200) = 'spxxx',
+				@fecha_inicio datetime = GETDATE(),
+				@num_registros int = NULL,
+				@id int = NULL,
+				@rc int,
+				@count_all int,
+				@count_ins int,
+				@idx_reclim int
 
+		--insertar en el log el comienzo de la ejecución de este SP
 		EXEC dbn1_norm_dhyf.audit.spn1_insertar_log
-					@p_id_carga		= @p_id_carga,
-					@p_bd			= @bd,
-					@p_esquema		= @esquema,
-					@p_objeto		= @objeto,
-					@p_fecha_inicio	= @fecha_inicio,
-					--@p_descripcion_warning solo informarlo en los SP de warnings, para el resto insertará null
-					@p_out_id		= @id OUT
+				@p_id_carga = @p_id_carga,
+				@p_bd = @bd,
+				@p_esquema = @esquema,
+				@p_objeto = @objeto,
+				@p_fecha_inicio = @fecha_inicio,
+				--@p_descripcion_warning solo informarlo en los SP de warnings, para el resto insertará null
+				@p_out_id = @id OUT
 
 		--ejecutar la lógica de negocio solo si todas las precondiciones están en estado 'OK'
 		IF dbn1_norm_dhyf.audit.fnn1_precondiciones_ok(@bd,@esquema,@objeto) = 1
@@ -38,4 +41,5 @@ BEGIN
 					@p_bd_precondicion = @bd,
 					@p_esquema_precondicion = @esquema,
 					@p_objeto_precondicion = @objeto
+
 --Fin Cabecera--
--- cambios_para_ssis_solo_pie.sql
+++ cambios_para_ssis_solo_pie.sql
@@ -1,8 +1,10 @@
 
 --Inicio Pie--
-			--guardar el valor de @@ROWCOUNT si no lo habíamos hecho ya
-			SET @rc = COALESCE(@rc,@@ROWCOUNT)
 
+			--el llamador de un DM no carga registros
+			SET @rc = 0
+
+			--actualizar la tabla de precondiciones para indicar que la ejecución de este SP ha finalizado 'OK' y que así los SP dependientes de él puedan lanzarse
 			DECLARE @fecha_fin_ok_precondicion datetime = GETDATE()
 			EXEC dbn1_norm_dhyf.audit.spn1_actualizar_tbn1_precondiciones_carga_dwh
 					@p_estado_precondicion = 'OK',
@@ -14,10 +16,10 @@
 
 		END
 
-		ELSE
+		ELSE --si no todas las precondiciones están en estado 'OK'
 		BEGIN
 
-			--actualizar la tabla de precondiciones para indicar que la ejecución de este SP queda pendiente y evitar que se lancen los SP dependientes de él
+			--actualizar la tabla de precondiciones para indicar que la carga de este SP queda pendiente y evitar que se lancen los SP dependientes de él
 			DECLARE @fecha_fin_pdte_precondicion datetime = GETDATE()
 			EXEC dbn1_norm_dhyf.audit.spn1_actualizar_tbn1_precondiciones_carga_dwh
 					@p_estado_precondicion = 'PDTE',
@@ -27,24 +29,24 @@
 					@p_esquema_precondicion = @esquema,
 					@p_objeto_precondicion = @objeto
 
-			--insertar warning avisando de que no se ha ejecutado la carga por no cumplirse todas las precondiciones
+			--insertar warning avisando de que este SP no ha cargado datos
 			DECLARE @id_warning_precondiones int
-			DECLARE @descripcion_warning_precondiciones varchar(4000) = @bd + '.' + @esquema + '.' + @objeto +
-																		' no ha cargado ningún registro porque no se han cumplido todas sus precondiciones'
+			DECLARE @descripcion_warning_precondiciones varchar(4000) = @bd + '.' + @esquema + '.' + @objeto + ' no ha cargado ningún registro porque no se han cumplido todas sus precondiciones'
 			EXEC dbn1_norm_dhyf.audit.spn1_insertar_log
-						@p_id_carga		= @p_id_carga,
-						@p_bd			= @bd,
-						@p_esquema		= @esquema,
-						@p_objeto		= @objeto,
-						@p_fecha_inicio	= @fecha_inicio,
-						@p_descripcion_warning = @descripcion_warning_precondiciones,
-						@p_out_id		= @id_warning_precondiones OUT
+					@p_id_carga = @p_id_carga,
+					@p_bd = @bd,
+					@p_esquema = @esquema,
+					@p_objeto = @objeto,
+					@p_fecha_inicio = @fecha_inicio,
+					@p_descripcion_warning = @descripcion_warning_precondiciones,
+					@p_out_id = @id_warning_precondiones OUT
 
 		END
 
+		--actualizar el log para indicar que este SP ha finalizado 'OK'
 		EXEC dbn1_norm_dhyf.audit.spn1_actualizar_log_correcto
-					@p_id = @id,
-					@p_num_registros = spvarxxx
+				@p_id = @id,
+				@p_num_registros = @rc
 
 	END TRY
 
@@ -60,21 +62,22 @@
 				@p_esquema_precondicion = @esquema,
 				@p_objeto_precondicion = @objeto
 
+		--actualizar el log para indicar que este SP ha finalizado con error
 		DECLARE @linea_error int,
 				@objeto_error varchar(200),
 				@descripcion_error varchar(4000)
-
 		SELECT	@linea_error = ERROR_LINE(),
 				@objeto_error = ERROR_PROCEDURE(),
 				@descripcion_error = SUBSTRING(ERROR_MESSAGE(),1,4000)
-
 		EXEC dbn1_norm_dhyf.audit.spn1_actualizar_log_error
-					@p_id = @id,
-					@p_linea_error = @linea_error,
-					@p_objeto_error = @objeto_error,
-					@p_descripcion_error = @descripcion_error
+				@p_id = @id,
+				@p_linea_error = @linea_error,
+				@p_objeto_error = @objeto_error,
+				@p_descripcion_error = @descripcion_error
 
-		/*si es carga de dim o fact añadir ;THROW*/
+		--elevar el error al padre para que vaya directamente al catch
+		;THROW
 
 	END CATCH
+
 --Fin Pie--
